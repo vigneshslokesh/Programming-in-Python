5cf3f0cd9dfc568665da158a71f2d83e2e20d931
vigneshslokesh <119556220+vigneshslokesh@users.noreply.github.com>
2024-07-09 00:25:14 +0530

    binary search with bubble sort

--- Programming in Python Intro.py
+++ Programming in Python Intro.py
@@ -1,4 +1,6 @@
 #bubble sort
+lst_us = [102, 5666, 2, 52, 98, 4, 1000]
+n = 102 
 
 def bubble(a):
     n = len(a)
@@ -9,8 +11,34 @@ def bubble(a):
                 tmp = a[j]
                 a[j] = a[j+1]
                 a[j+1] = tmp
-    print(a)
-            
-a = [5, 1, 4, 2, 8]
-bubble(a)
+    return a
+    
+print("Given List: ", lst_us)            
+lst = bubble(lst_us)
+print("Sorted List: ", lst)
 
+pos = -1
+
+def search(lst, n):
+    l = 0
+    u = len(lst) - 1
+
+    while l <= u:
+        mid = (l+u) // 2
+
+        if lst[mid] == n:
+            globals() ['pos'] = mid
+            return True
+        else:
+            if lst[mid] < n:
+                l = mid
+            else:
+                u = mid
+
+
+
+if search(lst, n):
+    print("Found at ", pos+1)
+
+else:
+    print("Not Found")
