21feaf6cf5fcaa30e608daf50898330983006477
vigneshslokesh <119556220+vigneshslokesh@users.noreply.github.com>
2024-07-19 21:19:00 +0530

    dynamic typing

---
+++ Dynamic typing.py
@@ -0,0 +1,15 @@
+#We are trying to illustrate what we call the dynamic typing
+
+a=10
+print(type(a))
+
+a='India'
+print(type(a))
+
+n=10
+print(type(n))
+print(n)
+n=n/2
+#when we use (/) division it converts it into float 
+print(type(n))
+print(n)
