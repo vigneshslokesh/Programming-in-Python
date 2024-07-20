65d2f399ae3e5d7ee7d0c777cb140f8355ff9767
vigneshslokesh <119556220+vigneshslokesh@users.noreply.github.com>
2024-07-20 22:26:43 +0530

    escape characters

--- escape chars and quotes.py
+++ escape chars and quotes.py
@@ -1,1 +1,15 @@
-print('Its\'s a beautiful day')
+print("Its\'s a beautiful day")
+# \ - backslash are used to escape a charcter in front of it
+
+print("We are from 'BIT' Bangalore")
+
+print("My name is Octa.\n\tI am from Bangalore")
+#\t - escape character called tab
+#\n - escape character called new line
+
+x = 'This is a string'
+y = 'This is also a string'
+z = 'first line'
+
+print(x)
+print(y)
