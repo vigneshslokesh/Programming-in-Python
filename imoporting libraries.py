dd36289ba892181c987f916c3e61b20f0f4dd8a6
vigneshslokesh <119556220+vigneshslokesh@users.noreply.github.com>
2024-07-24 23:23:49 +0530

    importung libraries

---
+++ imoporting libraries.py
@@ -0,0 +1,9 @@
+# imoporting libraries
+import math # math libraries - log(), pow()
+import random
+# random - randint, random, randrange
+
+import calendar #importing calendar
+
+# print(calendar.month(2024, 7)) # specific month
+print(calendar.calendar(2024)) # specific year
