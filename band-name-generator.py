ec2f1f4299502d15cea7aff16407c91466a5f295
vigneshslokesh <119556220+vigneshslokesh@users.noreply.github.com>
2024-07-30 23:33:03 +0530

    while loop

--- band-name-generator.py
+++ band-name-generator.py
@@ -1,14 +1,25 @@
 print("When did India get its independence (year)?")
 year = int(input())
 
-if (year==1947):
-    print("Great! you are correct.")
+# if (year==1947):
+#     print("Great! you are correct.")
 
-else:
-    print("Comeon dont you know this?")
-    print("Thats okay, I will give you a second chance.")
+# else:
+#     print("Comeon dont you know this?")
+#     print("Thats okay, I will give you a second chance.")
+#     year = int(input())
+#     if(year==1947):
+#         print("You got it right!")
+#     else:
+#         print("Failed in second attempt too?")
+
+#I would like to write a piece of code which lets the user as many times he wants.
+#The code will end, only when the user gets it right.
+
+while(year!=1947): #until this statement/condition becomes false
+    print("You got this wrong, Enter once again.")
     year = int(input())
-    if(year==1947):
-        print("You got it right!")
-    else:
-        print("Failed in second attempt too?")
+
+print("Wow! you got it right.")
+
+# while works like this
