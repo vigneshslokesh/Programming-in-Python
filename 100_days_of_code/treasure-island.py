f5caff2eb4f81d74635100d3595771c0bfad225f
vigneshslokesh <119556220+vigneshslokesh@users.noreply.github.com>
2024-08-02 00:03:55 +0530

    control statements and logical operators

--- 100_days_of_code/treasure-island.py
+++ 100_days_of_code/treasure-island.py
@@ -7,3 +7,23 @@ MM88MMM 8b,dPPYba,  ,adPPYba, ,adPPYYba, ,adPPYba, 88       88 8b,dPPYba,
   "Y888 88          `"Ybbd8"' `"8bbdP"Y8 `"YbbdP"'  `"YbbdP'Y8 88          
 
 ''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+
+crossover = input("You are in a crossover, Do you wanna go 'left' or 'right'?")
+if(crossover == "left"):
+    choice = input("There is a lake which has a island in centre, do you wanna 'swim' or 'wait' foor the boat?")
+    if(choice == "swim"):
+        door = input("You reached safely. There is a blue door, red door, yellow door, which one do you wanna open?")
+        if door == "blue":
+            print("Eaten by beasts. Game Over.")
+        elif door == "yellow":
+            print("You Win!")
+        elif door == "red":
+            print("Burned by fire. Game Over.")
+        else: 
+            print("Game Over.")
+    else:
+        print("Attacked by trout. Game Over.")
+else:
+    print("Fell into a hole. Game Over.")
