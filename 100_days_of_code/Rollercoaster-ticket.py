f40a6a28a550c7afbf377786376d3f07053e2d89
vigneshslokesh <119556220+vigneshslokesh@users.noreply.github.com>
2024-08-01 23:11:50 +0530

    if and elif conditions

---
+++ 100_days_of_code/Rollercoaster-ticket.py
@@ -0,0 +1,25 @@
+print("Welcome to Rollercoaster ticket vending machine")
+height = int(input("Enter your height in cm: "))
+bill = 0
+if(height>120):
+    print("You can ride the Rollercoaster")
+    age = int(input("Enter your age: "))
+    if(age < 12):
+        print("Child ticket price is $5.")
+        bill += 5
+    elif(age <= 18):
+        print("Youth ticket price is $7.")
+        bill += 7
+    else:
+        print("Adult ticket price is $12")
+        bill += 12
+    
+    wants_a_photo = input("Do you want a photo taken? Y or N. ")
+    if wants_a_photo == "Y":
+        print("Photo charges is $3.")
+        bill += 3
+
+    print(f"The final bill is ${bill}.")
+
+else:
+    print("Sorry, you don't have enough height to ride the Rollercoaster.")
