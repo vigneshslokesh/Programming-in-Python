17c25e5569c5733a0c42f9afa1561597c0c44acf
vigneshslokesh <119556220+vigneshslokesh@users.noreply.github.com>
2024-08-01 23:53:37 +0530

    treasure-island

---
+++ 100_days_of_code/treasure-island.py
@@ -0,0 +1,9 @@
+print('''  ,d                                                                       
+  88                                                                       
+MM88MMM 8b,dPPYba,  ,adPPYba, ,adPPYYba, ,adPPYba, 88       88 8b,dPPYba,  
+  88    88P'   "Y8 a8P_____88 ""     `Y8 I8[    "" 88       88 88P'   "Y8  
+  88    88         8PP""""""" ,adPPPPP88  `"Y8ba,  88       88 88          
+  88,   88         "8b,   ,aa 88,    ,88 aa    ]8I "8a,   ,a88 88          
+  "Y888 88          `"Ybbd8"' `"8bbdP"Y8 `"YbbdP"'  `"YbbdP'Y8 88          
+
+''')
