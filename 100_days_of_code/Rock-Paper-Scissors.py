bd118d0bf0d6fd4515bfb0cbd1ba852dfb65acb3
vigneshslokesh <119556220+vigneshslokesh@users.noreply.github.com>
2024-08-03 02:19:06 +0530

    random module

--- 100_days_of_code/Rock-Paper-Scissors.py
+++ 100_days_of_code/Rock-Paper-Scissors.py
@@ -25,3 +25,35 @@ scissors = '''
       (____)
 ---.__(___)
 '''
+print("Your choice")
+if choice == 0:
+    print(rock)
+elif choice == 1:
+    print(paper)
+else:
+    print(scissors)
+
+import random
+print("Computer choice")
+computer = random.randint(0,2)
+if computer == 0:
+    print(rock)
+elif computer == 1:
+    print(paper)
+else:
+    print(scissors)
+
+if choice == computer:
+    print("It's a draw")
+elif choice == 0 and computer == 1:
+    print("You lose")
+elif choice == 0 and computer == 2:
+    print("You win")
+elif choice == 1 and computer == 0:
+    print("You win")
+elif choice == 1 and computer == 2:
+    print("You lose")
+elif choice == 2 and computer == 0:
+    print("You lose")
+else:
+    print("You win")
