a87f7b2b40a1930a6f8d88f85b27db30b54f0ca6
vigneshslokesh <119556220+vigneshslokesh@users.noreply.github.com>
2024-08-03 02:43:22 +0530

    list and random module

--- 100_days_of_code/Rock-Paper-Scissors.py
+++ 100_days_of_code/Rock-Paper-Scissors.py
@@ -28,28 +28,31 @@ scissors = '''
 
 game_images = [rock,paper,scissors]
 choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors."))
-
-print("Your choice")
-print(game_images[choice])
-
-import random
-print("Computer choice")
-computer = random.randint(0,2)
-print(game_images[computer])
-
-if choice == computer:
-    print("It's a draw")
-elif choice>= 3 or choice < 0:
-    print("Invalid choice")
-elif choice == 0 and computer == 1:
-    print("You lose")
-elif choice == 0 and computer == 2:
-    print("You win")
-elif choice == 1 and computer == 0:
-    print("You win")
-elif choice == 1 and computer == 2:
-    print("You lose")
-elif choice == 2 and computer == 0:
-    print("You lose")
+if choice >= 3 or choice < 0:
+    print("Invalid choice, you lose!")
+    exit
 else:
-    print("You win")
+    print("Your choice")
+    print(game_images[choice])
+
+    import random
+    print("Computer choice")
+    computer = random.randint(0,2)
+    print(game_images[computer])
+
+    if choice == computer:
+        print("It's a draw")
+    elif choice>= 3 or choice < 0:
+        print("Invalid choice")
+    elif choice == 0 and computer == 1:
+        print("You lose")
+    elif choice == 0 and computer == 2:
+        print("You win")
+    elif choice == 1 and computer == 0:
+        print("You win")
+    elif choice == 1 and computer == 2:
+        print("You lose")
+    elif choice == 2 and computer == 0:
+        print("You lose")
+    elif choice == 2 and computer == 1:
+        print("You win")
