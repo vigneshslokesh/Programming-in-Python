a9f76e1e873f770b5f6ea5205c202ce85e52e710
vigneshslokesh <119556220+vigneshslokesh@users.noreply.github.com>
2024-08-26 21:57:42 +0530

    for loops

--- 100_days_of_code/for loops.py
+++ 100_days_of_code/for loops.py
@@ -10,8 +10,22 @@
 # print(max_score)
 
 # Range function
-total = 0
-for num in range(1,101):
-    total += num
+# total = 0
+# for num in range(1,101):
+#     total += num
 
-print(total)
+# print(total)
+
+# Odd and Even numbers
+while True:
+    num = int(input("Enter a number: "))
+
+    if num <= 0:
+        print("Enter a valid number")
+        
+    elif num % 2 == 0:
+        print("Even number")
+        
+    else:
+        print("Odd number")
+        
