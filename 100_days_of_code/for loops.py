e623acc130980e536c7bc55813d60abbd51e0878
vigneshslokesh <119556220+vigneshslokesh@users.noreply.github.com>
2024-08-26 21:36:13 +0530

    gause num

--- 100_days_of_code/for loops.py
+++ 100_days_of_code/for loops.py
@@ -1,10 +1,17 @@
-student_scores = [150, 142, 185, 120, 171, 184, 149, 2, 59, 68, 199, 78, 65, 89]
+# maximum of a set of numbers in a list
 
+# student_scores = [150, 142, 185, 120, 171, 184, 149, 2, 59, 68, 199, 78, 65, 89]
 
+# max_score = 0
+# for score in student_scores:
+#     if score > max_score:
+#         max_score = score
 
-max_score = 0
-for score in student_scores:
-    if score > max_score:
-        max_score = score
+# print(max_score)
 
-print(max_score)
+# Range function
+total = 0
+for num in range(1,101):
+    total += num
+
+print(total)
