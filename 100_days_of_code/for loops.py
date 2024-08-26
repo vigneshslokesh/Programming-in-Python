169dd2938cf1c27d299710995067690e913385a0
vigneshslokesh <119556220+vigneshslokesh@users.noreply.github.com>
2024-08-26 21:24:06 +0530

    for loops

--- 100_days_of_code/for loops.py
+++ 100_days_of_code/for loops.py
@@ -1,4 +1,8 @@
 student_scores = [150, 142, 185, 120, 171, 184, 149, 2, 59, 68, 199, 78, 65, 89]
 
 total_score = sum(student_scores)
-print(total_score)
+sum = 0
+for score in student_scores:
+    sum += score
+
+print(sum)
