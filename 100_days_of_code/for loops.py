f07bcee1d7f0832c99a16bbfbf2eb8047cfdac5f
vigneshslokesh <119556220+vigneshslokesh@users.noreply.github.com>
2024-08-26 21:27:38 +0530

    max of numbs

--- 100_days_of_code/for loops.py
+++ 100_days_of_code/for loops.py
@@ -1,8 +1,10 @@
 student_scores = [150, 142, 185, 120, 171, 184, 149, 2, 59, 68, 199, 78, 65, 89]
 
-total_score = sum(student_scores)
-sum = 0
-for score in student_scores:
-    sum += score
 
-print(sum)
+
+max_score = 0
+for score in student_scores:
+    if score > max_score:
+        max_score = score
+
+print(max_score)
