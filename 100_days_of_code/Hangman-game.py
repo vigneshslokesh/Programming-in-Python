b27723d4f99ffeecc7e41b447af591ad94cd7fb0
vigneshslokesh <119556220+vigneshslokesh@users.noreply.github.com>
2024-08-28 23:32:15 +0530

    hangman game

--- 100_days_of_code/Hangman-game.py
+++ 100_days_of_code/Hangman-game.py
@@ -1,9 +1,16 @@
 import random
 word_list = ["lorey","james","mermaid"]
 
-rand_num = random.randint(0,2)
-chosen_word = word_list[rand_num]
-print(chosen_word)
+# rand_num = random.randint(0,2)
+# chosen_word = word_list[rand_num]
+
+#random
+#randint
+#choice
+#shuffle
+
+chosen_word = random.choice(word_list) # more line saving and efficient
+print(chosen_word) 
 
 guess = input("Guess a letter: ").lower()
 if guess in chosen_word:
