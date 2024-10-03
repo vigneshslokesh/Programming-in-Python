349fa440bbad0d009613a19abc0ebee53805c880
vigneshslokesh <119556220+vigneshslokesh@users.noreply.github.com>
2024-10-03 23:22:00 +0530

    ceaser cipher part2

--- 100_days_of_code/ceaser cipher.py
+++ 100_days_of_code/ceaser cipher.py
@@ -6,33 +6,27 @@ shift = int(input("Type the shift number:\n"))
 
 
 # TODO-1: Create a function called 'encrypt()' that takes 'original_text' and 'shift_amount' as 2 inputs.
-
 # TODO-2: Inside the 'encrypt()' function, shift each letter of the 'original_text' forwards in the alphabet
 #  by the shift amount and print the encrypted text.
 
-
-def encrypt(original_text, shift_amount):
-    encoded =''
-    for letter in original_text:
-        shifted_pos = alphabet.index(letter) + shift_amount    
-        shifted_pos = shifted_pos % len(alphabet)   # mod functionality loop inside the list to overcome out of range error
-        encoded += alphabet[shifted_pos]
-
-    print(f'Here is the encode result: {encoded}')
-
-encrypt(original_text=text,shift_amount=shift)   
 
 # TODO-1: Create a function called 'decrypt()' that takes 'original_text' and 'shift_amount' as inputs.
 # TODO-2: Inside the 'decrypt()' function, shift each letter of the 'original_text' *backwards* in the alphabet
 #  by the shift amount and print the decrypted text.
 
-def decrypt(original_text, shift_amount):
-    decoded =''
+
+
+def ceaser(original_text,shift_amount, encode_decode):
+    output_text =''
     for letter in original_text:
-        shifted_pos = alphabet.index(letter) - shift_amount    
+        if encode_decode == 'encode':
+            shifted_pos = alphabet.index(letter) + shift_amount  
+        elif encode_decode == 'decode':
+            shifted_pos = alphabet.index(letter) - shift_amount
         shifted_pos = shifted_pos % len(alphabet)   # mod functionality loop inside the list to overcome out of range error
-        decoded += alphabet[shifted_pos]
+        output_text += alphabet[shifted_pos]
 
-    print(f'Here is the decoded result: {decoded}')
+    print(f'Here is the {encode_decode}d result: {output_text}')
+    pass
 
-decrypt(original_text=text,shift_amount=shift)
+ceaser(original_text=text,shift_amount=shift,encode_decode=direction)
