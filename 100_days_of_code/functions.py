9d2182db9e0b50f7ecbfe9cdbc96b9e9dbafae80
vigneshslokesh <119556220+vigneshslokesh@users.noreply.github.com>
2024-10-03 22:06:45 +0530

    functions in python

--- 100_days_of_code/functions.py
+++ 100_days_of_code/functions.py
@@ -8,8 +8,18 @@
 
 # function with inputs
 
-def greet_with_name(name):
-    print("Hello "+name)
-    print(f"How do you do {name}?")
+# def greet_with_name(name):
+#     print("Hello "+name)
+#     print(f"How do you do {name}?")
 
-greet_with_name("Vignesh")
+# greet_with_name("John")
+
+# function with multiple inputs
+
+def greet_with(name,location):
+    print("Hi "+name)
+    print(f"How's the weather in {location}")
+
+greet_with(location="John",name="Georgia")
+
+#positional argument and Keyword argument
