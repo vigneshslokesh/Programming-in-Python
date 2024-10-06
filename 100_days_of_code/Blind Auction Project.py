1deff9cfa52b0421ef9bac3c51920526456a35c7
vigneshslokesh <119556220+vigneshslokesh@users.noreply.github.com>
2024-10-06 23:08:36 +0530

    auction using dictionary

--- 100_days_of_code/Blind Auction Project.py
+++ 100_days_of_code/Blind Auction Project.py
@@ -4,7 +4,7 @@ cont = True
 while cont:
     
     name = input("What is your name?: ")
-    bid = int(input("What's your bid? "))
+    bid = int(input("What's your bid? Rs"))
     others = input("Are there any other bidders? ").lower()
     print("\n" * 5)
     bidders[name] = bid
@@ -15,6 +15,7 @@ while cont:
         for item in bidders:
             print(f"Name: {item}")
             print(f"Bid: {bidders[item]}")
-            
+
+        person = max(bidders, key=bidders.get)   
         highest = max(bidders.values())  # Correct usage of max()
-        print(f"The highest bid is: {highest}")
+        print(f"The highest bid was from {person} with: Rs{highest}")
