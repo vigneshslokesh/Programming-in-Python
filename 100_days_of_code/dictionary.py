5151646bf75c81fb2487e727dfdcb0388c3fab31
vigneshslokesh <119556220+vigneshslokesh@users.noreply.github.com>
2024-10-06 20:21:57 +0530

    dictionary in python

--- 100_days_of_code/dictionary.py
+++ 100_days_of_code/dictionary.py
@@ -24,3 +24,30 @@ print(programming_dictionary["Bug"])
 for obj in programming_dictionary:
     print(obj)
     print(programming_dictionary[obj])
+
+# Excercise on dictionary
+student_scores = {
+    'Harry': 88,
+    'Ron': 78,
+    'Hermione': 95,
+    'Draco': 75,
+    'Neville': 60
+}
+
+student_grades = {}
+
+for students in student_scores:
+    score = student_scores[students]
+    
+    if score >= 91:
+        student_grades[students] = 'Outstanding'
+    
+    elif score >= 81:
+        student_grades[students] = 'Exceeds Expectations'
+        
+    elif score >= 71:
+        student_grades[students] = 'Acceptable'
+        
+    else :
+        student_grades[students] = 'Fail'
+
