85b79edd906a2c5f6506310fe77639735b566529
vigneshslokesh <119556220+vigneshslokesh@users.noreply.github.com>
2024-10-07 22:42:09 +0530

    function with return

--- 100_days_of_code/Functions with outputs.py
+++ 100_days_of_code/Functions with outputs.py
@@ -1,8 +1,21 @@
+
+# this function returns the variable to where the function was called
 def format_name(f_name,l_name):
     first = f_name.title()
     last = l_name.title()
     full_name = first +' '+last
-    print(full_name)
+    return full_name
 
-format_name('vignesh','ReddY')
+formatted_string = format_name('vignesh','ReddY')
+print(formatted_string)
+
+
+# function to count the length in a text   
+def length(param):
+    count = 0
+    for i in param:
+        count += 1
+    return count    
     
+out = length("Vig")
+print(out)
