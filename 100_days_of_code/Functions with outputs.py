8e024cebe8cfa39a39c91e9c780e51e36ca3e245
vigneshslokesh <119556220+vigneshslokesh@users.noreply.github.com>
2024-10-07 23:16:15 +0530

    functions with return

--- 100_days_of_code/Functions with outputs.py
+++ 100_days_of_code/Functions with outputs.py
@@ -1,21 +1,32 @@
 
-# this function returns the variable to where the function was called
-def format_name(f_name,l_name):
+# # this function returns the variable to where the function was called
+# def format_name(f_name,l_name):
+#     first = f_name.title()
+#     last = l_name.title()
+#     full_name = first +' '+last
+#     return full_name
+
+# formatted_string = format_name('vignesh','ReddY')
+# print(formatted_string)
+
+
+# # function to count the length in a text   
+# def length(param):
+#     count = 0
+#     for i in param:
+#         count += 1
+#     return count    
+    
+# out = length("Vig")
+# print(out)
+
+# function with multiple return
+def full_name(f_name,l_name):
+    if f_name =='' and l_name == '':
+        return ("Enter a valid name!")
     first = f_name.title()
     last = l_name.title()
-    full_name = first +' '+last
-    return full_name
+    return (f"{first} {last}")
 
-formatted_string = format_name('vignesh','ReddY')
-print(formatted_string)
-
-
-# function to count the length in a text   
-def length(param):
-    count = 0
-    for i in param:
-        count += 1
-    return count    
-    
-out = length("Vig")
-print(out)
+name = full_name(input("Enter your first name: "),input("Enter your last name: "))
+print(name)
