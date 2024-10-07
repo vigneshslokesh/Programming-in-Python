4a9d685cfede5aec05f712cffca588109bb042b0
vigneshslokesh <119556220+vigneshslokesh@users.noreply.github.com>
2024-10-07 22:31:44 +0530

    functions op

--- 100_days_of_code/Functions with outputs.py
+++ 100_days_of_code/Functions with outputs.py
@@ -1,2 +1,8 @@
-def format_name():
+def format_name(f_name,l_name):
+    first = f_name.title()
+    last = l_name.title()
+    full_name = first +' '+last
+    print(full_name)
+
+format_name('vignesh','ReddY')
     
