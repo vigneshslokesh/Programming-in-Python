f91d806e064c038c6670d5a8e9fe36ac059cde56
vigneshslokesh <119556220+vigneshslokesh@users.noreply.github.com>
2024-10-14 22:01:12 +0530

    phase 2

--- 100_days_of_code/Blackjack.py
+++ 100_days_of_code/Blackjack.py
@@ -24,13 +24,24 @@ computer_cards = []
 is_game_over = False
 
 for _ in range(2):
-    user_cards.append(deal_card)
-    computer_cards.append(deal_card)
+    user_cards.append(deal_card())
+    computer_cards.append(deal_card())
 
-user_scr = calculate_score(user_cards)
-computer_scr = calculate_score(computer_cards)
-print(f"Your cards: {user_cards}, current score: {user_scr}")
-print(f"Computer's first card: {computer_cards[0]}")
+while not is_game_over:
+    user_scr = calculate_score(user_cards)
+    computer_scr = calculate_score(computer_cards)
+    print(f"Your cards: {user_cards}, current score: {user_scr}")
+    print(f"Computer's first card: {computer_cards[0]}")
 
-if user_scr == 0 or computer_scr == 0 or user_scr > 21:
-    is_game_over = True
+    if user_scr == 0 or computer_scr == 0 or user_scr > 21:
+        is_game_over = True
+    else:
+        user_should_deal = input("Type 'y' to get another card, type 'n' to pass: ")
+        if user_should_deal == 'y':
+            user_cards.append(deal_card())
+        else:
+            is_game_over = True
+
+while computer_scr != 0 and computer_scr < 17:
+    computer_cards.append(deal_card())
+        
