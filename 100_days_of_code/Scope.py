ecc6efbe4e8b0318871c45e41cf0f97c06dfb771
vigneshslokesh <119556220+vigneshslokesh@users.noreply.github.com>
2024-10-15 22:55:42 +0530

    global constants

--- 100_days_of_code/Scope.py
+++ 100_days_of_code/Scope.py
@@ -1,12 +1,21 @@
-# global scope
-enemies = 1
+# # global scope
+# enemies = 1
 
-def new_enemies(enemy):
-    # global enemies
-    # enemies += 1
-    print(f"Inside Enemies: {enemies}")
-    return enemy + 1
+# def new_enemies(enemy):
+#     # global enemies
+#     # enemies += 1
+#     print(f"Inside Enemies: {enemies}")
+#     return enemy + 1
 
-ne = new_enemies(enemies)
-print(ne)
-print(f"Outside Enemies: {enemies}")
+# ne = new_enemies(enemies)
+# print(ne)
+# print(f"Outside Enemies: {enemies}")
+
+# Global constants
+PI = 3.14159
+GOOGLE_URL = "www.google.com"
+
+def my_func():
+    print(GOOGLE_URL)
+
+my_func()
