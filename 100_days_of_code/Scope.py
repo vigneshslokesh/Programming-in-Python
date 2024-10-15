8774ee07d1e5ccf11aac668225a67c87f343599c
vigneshslokesh <119556220+vigneshslokesh@users.noreply.github.com>
2024-10-15 22:51:20 +0530

    modify scopes

--- 100_days_of_code/Scope.py
+++ 100_days_of_code/Scope.py
@@ -1,10 +1,12 @@
 # global scope
 enemies = 1
 
-def new_enemies():
-    global enemies
-    enemies += 1
+def new_enemies(enemy):
+    # global enemies
+    # enemies += 1
     print(f"Inside Enemies: {enemies}")
+    return enemy + 1
 
-new_enemies()
+ne = new_enemies(enemies)
+print(ne)
 print(f"Outside Enemies: {enemies}")
