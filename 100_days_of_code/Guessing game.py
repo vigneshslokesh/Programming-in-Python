1e019163104833b306e47f619e0cb803cfbd4830
vigneshslokesh <119556220+vigneshslokesh@users.noreply.github.com>
2024-10-15 23:43:11 +0530

    scopes in python

--- 100_days_of_code/Guessing game.py
+++ 100_days_of_code/Guessing game.py
@@ -1,5 +1,31 @@
 import random
 
+
+def predictor(attempts):
+    while attempts>0:
+        print(f"You have {attempts} attempts left to guess a number.")
+        print(f"Computer number is {the_num}")
+        guess = int(input("Make a guess: "))
+        if guess == the_num:
+            print(f"Your guess is correct: {guess}")
+            break
+        elif guess!= the_num:          
+            if guess < the_num:
+                print("Too low.")
+            elif guess > the_num:
+                print("Too high")
+            attempts -= 1
+            
+            if attempts>0:
+                print("Guess again")
+            
+    if attempts ==0:
+        print("You loose, out of attempts!")
+    else:
+        print("You Win!!")
+
+
+
 print("Welcome to the Number Guessing Game!")
 print("I'm thinking of a number between 1 to 100.")
 the_num = random.randint(1,100)
@@ -7,21 +33,7 @@ the_num = random.randint(1,100)
 difficulty = input("Choose a difficulty, Type 'easy' or 'hard': ").lower()
 if difficulty == "easy":
     attempts = 5
-    while attempts==0:
-        print(f"You have {attempts} attempts left to guess a number.")
-        guess = int(input("Make a guess: "))
-        if guess == the_num:
-            print(f"Your guess is correct: {guess}")
-        elif guess!= the_num:
-            
-            if guess < the_num:
-                print("Too low.")
-            elif guess > the_num:
-                print("Too high")
-            print("Guess again")
-            attempts -= 1
-    
-    
-# else:
-#     attempts = 3
-#     pass
+    predictor(attempts)
+else:
+    attempts = 10
+    predictor(attempts)
