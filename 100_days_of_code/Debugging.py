c524f2b89548eab7218b0022c9ac37614ea8d7b5
vigneshslokesh <119556220+vigneshslokesh@users.noreply.github.com>
2024-10-17 12:54:09 +0530

    describe the problem

--- 100_days_of_code/Debugging.py
+++ 100_days_of_code/Debugging.py
@@ -1,9 +1,11 @@
 def my_function():
-    for i in range(1,21):
+    for i in range(1,20):
         if i == 20:
             print("You got it")
 
 my_function()
+
+
 
 #Describe the problem - Write your answers as comments:
 # 1. What is the for loop doing?
@@ -12,3 +14,17 @@ my_function()
 # When the i is equal to 20
 # 3. What are your assumptions about the value of i? 
 # The i iterates only till 19 because range function is set from 1 to 20 which excludes the last number of the range
+
+# def my_function():
+#     for i in range(1,20):
+#         pass
+#     print("You got it")
+
+def my_function():
+    for i in range(1,21):
+        pass
+    print("You got it")
+
+my_function()
+
+# if you still want to print "You got it"
