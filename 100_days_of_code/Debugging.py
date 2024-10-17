6069e3b3e8ca79677254f32795fb5ddc98a09da7
vigneshslokesh <119556220+vigneshslokesh@users.noreply.github.com>
2024-10-17 12:51:30 +0530

    Debugging

---
+++ 100_days_of_code/Debugging.py
@@ -0,0 +1,14 @@
+def my_function():
+    for i in range(1,21):
+        if i == 20:
+            print("You got it")
+
+my_function()
+
+#Describe the problem - Write your answers as comments:
+# 1. What is the for loop doing?
+#  The for loop iterates i from 1 to 19 
+# 2. When is the function meant to print "You got it"?
+# When the i is equal to 20
+# 3. What are your assumptions about the value of i? 
+# The i iterates only till 19 because range function is set from 1 to 20 which excludes the last number of the range
