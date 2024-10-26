5f0b1dcd69f931a34944243610cf443250f103bc
vigneshslokesh <119556220+vigneshslokesh@users.noreply.github.com>
2024-10-26 13:48:29 +0530

    game

--- 100_days_of_code/Higher or Lower Project/main.py
+++ 100_days_of_code/Higher or Lower Project/main.py
@@ -1,48 +1,23 @@
+from art import logo, vs
 from game_data import data
 import random
 
-# print(data[0])
-def random_gen():
-    return random.choice(data)
+random_gen = random.choice(data)
 
-comp_a = random_gen()
-comp_b = random_gen()
+def formattable(account):
+    account_name = account["name"]
+    account_count = account["follower_count"]
+    account_desc = account["decription"]
+    account_country = account["country"]
 
-while comp_b == comp_a:
-    comp_b = random_gen()
-
-# print(comp_a)
-# print(comp_b)
-
-score = 0
-
-def compare(choice, comp_a, comp_b):
-    if comp_a['follower_count'] > comp_b['follower_count']:
-        ans = 'a'
-        # comp_b = random_gen()
-    else:
-        ans = 'b'
-        # comp_a = comp_b
-        # comp_b = random_gen()
-    return choice == ans
+    return f"{account_name}, a {account_desc}, from {account_country}"
 
 
-    
-print(comp_a)
-print(comp_b)
+account_a = random.choice(data)
+account_b = random.choice(data)
 
+if account_a == account_b:
+    account_b = random.choice(data)
 
-while True:
-    print(f"Compare A: {comp_a['name']}, a {comp_a['description']}, from {comp_a['country']}")
-    print(f"Compare B: {comp_b['name']}, a {comp_b['description']}, from {comp_b['country']}")
-
-    choice = input("Who has more followers? Type 'A' or 'B': ").lower()
-
-    if compare(choice, comp_a, comp_b):
-        score += 1
-        print(f'Correct! Your score: {score}')
-        comp_a = comp_b
-        comp_b = random_gen()
-    else:
-        print(f"Wrong answer, your score: {score}")
-        break
+print(f"Compare A: {formattable(account_a)}")
+print(f"Compare B: {formattable(account_b)}")
