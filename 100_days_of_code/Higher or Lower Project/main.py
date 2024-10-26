dcda0cf0a987270e183c2caa58e97cb8b4c6be1e
vigneshslokesh <119556220+vigneshslokesh@users.noreply.github.com>
2024-10-26 13:59:47 +0530

    game phase 2

--- 100_days_of_code/Higher or Lower Project/main.py
+++ 100_days_of_code/Higher or Lower Project/main.py
@@ -7,11 +7,19 @@ random_gen = random.choice(data)
 def formattable(account):
     account_name = account["name"]
     account_count = account["follower_count"]
-    account_desc = account["decription"]
+    account_desc = account["description"]
     account_country = account["country"]
 
     return f"{account_name}, a {account_desc}, from {account_country}"
 
+
+def check_answer(user_guess, a_follower, b_follower):
+    if a_follower > b_follower:
+        return user_guess == "a"
+    else:
+        return user_guess == "b "
+    
+    pass
 
 account_a = random.choice(data)
 account_b = random.choice(data)
@@ -21,3 +29,10 @@ if account_a == account_b:
 
 print(f"Compare A: {formattable(account_a)}")
 print(f"Compare B: {formattable(account_b)}")
+
+guess = input("Who has more followes? Type 'A' or 'B': ").lower()
+
+a_follower_count = account_a["follower_count"]
+b_followe_count = account_b["follower_count"]
+
+is_correct = check_answer(guess,a_follower_count,b_followe_count)
