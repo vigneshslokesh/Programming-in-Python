0c3c48106d5e7ee491105d9d53f65977f831c0e1
vigneshslokesh <vigneshslokesh0612@gmail.com>
2025-05-28 15:55:43 +0530

    Turtle module - Square

---
+++ Turtle GUI/Turtle Square.py
@@ -0,0 +1,16 @@
+from turtle import Turtle, Screen
+
+turtle = Turtle()
+turtle.shape("turtle")
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+
+
+screen = Screen()
+screen.onscreenclick(exit)
