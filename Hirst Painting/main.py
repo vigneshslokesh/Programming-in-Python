f7e442cffc75e1812b1067da4a315e2e50584d61
vigneshslokesh <vigneshslokesh0612@gmail.com>
2025-05-29 16:05:30 +0530

    Hirst Painting

---
+++ Hirst Painting/main.py
@@ -0,0 +1,16 @@
+import colorgram
+
+rgb_colors = []
+colors = colorgram.extract('image.jpg',15)
+
+# print(colors)
+for color in colors:
+    r = color.rgb.r
+    g = color.rgb.g
+    b = color.rgb.b
+    new_color = (r, g, b)
+    rgb_colors.append(new_color)
+
+print(rgb_colors)
+
+[(253, 253, 252), (242, 244, 247), (241, 247, 243), (144, 76, 50), (188, 165, 117), (248, 244, 246), (166, 153, 36), (14, 46, 85), (139, 185, 176), (146, 56, 81), (42, 110, 136), (59, 120, 99), (145, 170, 177), (87, 35, 30), (64, 152, 169)]
