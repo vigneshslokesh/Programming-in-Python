6074878a8ec7407330479054aeab54630e43e38e
vigneshslokesh <vigneshslokesh0612@gmail.com>
2025-05-29 12:38:42 +0530

    turtle shapes

--- Turtle GUI/Turtle Shapes.py
+++ Turtle GUI/Turtle Shapes.py
@@ -1,7 +1,20 @@
 from turtle import Turtle,Screen
+import random
 
 turtle = Turtle()
 
-turtle.forward(100)
-turtle.degrees(90)
-turtle.forward(100)
+colour = ["red", "black", "pink", "blue", "green", "orange", "grey", "purple", "coral"]
+def draw_shape(num):
+    angle = 360 / num
+    for _ in range(num):
+        turtle.right(angle)
+        turtle.forward(100)
+
+
+for i in range(3,11):
+    turtle.color(random.choice(colour))
+    draw_shape(i)
+
+
+screen = Screen()
+screen.exitonclick()
