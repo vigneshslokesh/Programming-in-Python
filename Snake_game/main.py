dcec6b40617934d705394edff78b85bbb0f716f0
vigneshslokesh <vigneshslokesh0612@gmail.com>
2025-07-18 18:44:34 +0530

    Snake Game - Create and Move

--- Snake_game/main.py
+++ Snake_game/main.py
@@ -1,10 +1,12 @@
 from turtle import Screen, Turtle
+import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
 
 screen.bgcolor("black")
 screen.title("My Snake Game")
+screen.tracer(0)
 
 starting_position = [(0,0),(-20,0),(-40,0)]
 segments = []
@@ -16,11 +18,21 @@ for pos in starting_position:
     new_segment.goto(pos)
     segments.append(new_segment)
 
+
+screen.update()
+
 game_is_on = True
 
 while game_is_on:
-    for seg in segments:
-        seg.forward(20)
+    screen.update()
+    time.sleep(0.2)
+    for seg in range(len(segments)-1, 0, -1):
+        new_x = segments[seg-1].xcor()
+        new_y = segments[seg-1].ycor()
+        segments[seg].goto(new_x,new_y)
+    segments[0].forward(20)
+        
+        
         
 
 # print(segments)
