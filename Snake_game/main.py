4b1a36eb1372db72582b407369f06961555c1c81
vigneshslokesh <vigneshslokesh0612@gmail.com>
2025-07-19 20:04:36 +0530

    Snake_game - food module

--- Snake_game/main.py
+++ Snake_game/main.py
@@ -1,5 +1,6 @@
 from turtle import Screen, Turtle
 from snake import Snake
+from food import Food
 import time
 
 screen = Screen()
@@ -10,6 +11,7 @@ screen.title("My Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
 
 screen.listen()
 screen.onkey(snake.up,"Up")
@@ -21,8 +23,12 @@ game_is_on = True
 
 while game_is_on:
     screen.update()
-    time.sleep(0.2)
+    time.sleep(0.13)
     snake.move()
+
+    if snake.head.distance(food) < 15:
+        print("collision")
+        food.refresh()
     
 
 screen.exitonclick()       
